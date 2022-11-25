650dfaf07e9ac8428cf3d5666e97f0b2a5d8fd16
joy <juniorbat0003@gmail.com>
2022-11-25 15:48:00 +0600

    Database connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -11,7 +12,18 @@ app.use(cors());
 app.use(express.json())
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xloqa3g.mongodb.net/?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
 app.get('/', async (req, res) => {
+    console.log(uri)
     res.send('server is running')
 })
 
